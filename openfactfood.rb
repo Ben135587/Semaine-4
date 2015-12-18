c2deac53d5ae62f43ed73b830e1842a6bb22e265
Benoit <bellet.benoit@gmail.com>
2015-12-18 22:08:35 +0000

    openfactfood.rb

--- openfactfood.rb
+++ openfactfood.rb
@@ -3,12 +3,14 @@ require 'json'
 
 loop do
     puts "Entrez un numero de code barre :"
-    bar_code = gets.champ
+    bar_code = gets.chomp
 
     url="http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
 
     json = open(url).read
 
-    date = JSON.parse(json)
-    puts data[*product*][*product_name*]
+    data = JSON.parse(json)
+    
+    puts data['product']['product_name']
+    
 end
